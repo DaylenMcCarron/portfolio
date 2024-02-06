148c37ce3dbc558c19e11f6a48a5ce3da9a74651
Daylen McCarron <daylenricardomccarron@gmail.com>
2024-02-07 03:20:24 +0530

    add: make tech_icons move as mouse moves

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,6 +1,14 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className=" z-50 backdrop-blur-sm fixed border-b-[1px] border-slate-600 shadow-md w-screen text-white">
+      <ul className=" max-w-7x1 flex items-baseline justify-end space-x-16 mr-16 p-6">
+        <li>About Me</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Resume</li>
+        <li>Contact</li>
+      </ul>
+    </div>
   )
 }
 
--- src/home/Display.tsx
+++ src/home/Display.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+interface Person {
+  [index:string]: string
+
+}
 
 type Props = {}
 
 const Display = (props: Props) => {
+  
+  let posx = ''
+  document.addEventListener('mousemove', (e) => {
+    let posx = e.pageX/15
+    let posy = e.pageY/15
+    document.getElementById('js_icon')?.setAttribute("style",`left:${200+posx}px;top:${100+posy}px`)
+    document.getElementById('ts_icon')?.setAttribute("style",`left:${700+posx}px;top:${150+posy}px`)
+    document.getElementById('java_icon')?.setAttribute("style",`left:${300+posx}px;top:${200+posy}px`)
+    document.getElementById('react_icon')?.setAttribute("style",`left:${650+posx}px;top:${250+posy}px`)
+    
+
+    // js = `${200 + x/10}px`
+  })
+
   return (
-    <div>Display</div>
+    <div className=' h-screen text-cyan-50 bg-gradient-to-br from-black via-70% via-slate-800 to-slate-900'>
+      <div className=' w-[100vw] h-[100vh absolutebottom-2/4' id="demo">
+        <div id='js_icon' className={` w-12 h-12 bg-cyan-500 absolute top-[100px] rounded-md hover:shadow-2xl hover:shadow-white hover:border-white hover:border-2`}></div>
+        <div id='ts_icon' className={` w-10 h-10 bg-cyan-500 absolute top-[100px]`}></div>
+        <div id='java_icon' className={` w-10 h-10 bg-cyan-500 absolute top-[100px]`}></div>
+        <div id='react_icon' className={` w-10 h-10 bg-cyan-500 absolute top-[100px]`}></div>
+      </div>
+    </div>
   )
 }
 
