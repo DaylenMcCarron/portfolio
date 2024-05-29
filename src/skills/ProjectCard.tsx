8ba13e1963f6c79b238002fc4046f6f16ef91b08
Daylen McCarron <daylenricardomccarron@gmail.com>
2024-05-29 14:34:11 +0530

    add: Updated Projects

--- src/skills/ProjectCard.tsx
+++ src/skills/ProjectCard.tsx
@@ -1,7 +1,18 @@
 import {useEffect, useState } from "react"
 
+interface ProjectData {
+  name: string;
+  description: string;
+  link: string;
+  technology: string;
+  imageName: string;
+}
 
-const ProjectCard = () => {
+interface ProjectCardProps {
+  projectData: ProjectData;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ projectData }) => {
   const [picNum, setPicNum] = useState(1);
 
 const changePic = () => {
@@ -24,15 +35,15 @@ useEffect(() => {
   
   
   return (
-    <div className='h-[80%] w-[50vw] border-[1px] mx-auto m-20 border-slate-400 rounded-3xl glassbg shadow-2xl shadow-slate-900' >
+    <div className='h-fit w-[50vw] border-[1px] mx-auto m-20 border-slate-400 rounded-3xl glassbg shadow-2xl shadow-slate-900 hover:shadow-slate-600 duration-200' >
       <div className='w-[98%] h-[80%]  m-auto mt-3 rounded-t-2xl'>
-        <img src={`/assets/aaad${picNum}.png`} className=" object-contain w-full h-full bg-black m-auto mt-3 rounded-2xl border-slate-500 border-[1px]" alt="" />
+        <img src={`/assets/${projectData.imageName}${picNum}.png`} className=" object-contain w-full h-full bg-black m-auto mt-3 rounded-2xl border-slate-500 border-[1px]" alt="" />
       </div>
       <div>
-        <h2 className="text-white mx-6 my-1 font-bold text-xl">An-Apple-A-Day - (Hackathon project)</h2>
-        <p className="text-white mx-6 font-light">An AI tool that can help you with diagnosis of provided symptoms</p>
-        <h2 className="text-white mx-8 text-md mt-3">HTML, CSS, Javascript, React</h2>
-        <h2>Link</h2>
+        <h2 className="text-white mx-6 my-1 font-bold text-xl">{projectData.name}</h2>
+        <p className="text-white mx-6 font-light">{projectData.description}</p>
+        <h2 className="text-white mx-8 text-md my-3">{projectData.technology}</h2>
+        <a href={projectData.link} target="_blank" className=" text-sky-200 ml-4 font-bold bg-[#ffffff22] border-white border-[1px] rounded-b-2xl p-3 right-[30%] absolute ">Open Project</a>
       </div>
     </div>
   )
--- src/skills/Projects.tsx
+++ src/skills/Projects.tsx
@@ -1,15 +1,19 @@
 
+import { describe } from 'node:test';
 import ProjectCard from './ProjectCard'
 
 type Props = {}
 
 function Projects({}: Props) {
+  const Project1 = {name:"An-Apple-A-Day - (Hackathon project)",description:" An-Apple-A-Day is a website where users can enter their symptoms to identify potential health issues with the help of AI",link:"https://github.com/siripiripiri/JavaChips",technology:"It is built using React, Firebase, Flask, and the Gemma Model, with Vite for tooling", imageName:"aaad"};
+  const Project2 = {name:"The Coffee Place - (Live Website)",description:"The Coffee Place is an online store where users can purchase coffee. ",link:"https://thecoffeeplace.vercel.app",technology:"The website is built using HTML, CSS, JavaScript, Tailwind, and React.",imageName:"tcp"};
+  const Project3 = {name:" QuickNoms - Q-Commerce Platfrom - (College Project)",description:"QuickNoms is a Q-Commerce platform that consists of a user, restaurant and delivery partner app.",link:"https://github.com/DaylenMcCarron/QuickNoms",technology:"It has been created using Flutter and Dart for the app, Firebase RTDB, Firestore, FCM, Firebase Cloud Storage",imageName:"qn"};
   return (
     <div className='h-fit w-[100vw]'><h1 className='text-center text-6xl text-sky-200 sixtyfour py-2 shadow-white pt-12' id='Projects'>Projects</h1>
      <div className='flex w-full h-fit  flex-col  pt-8'>
-     <ProjectCard />
-     <ProjectCard />
-     <ProjectCard />
+     <ProjectCard projectData={Project1}/>
+     <ProjectCard projectData={Project2}/>
+     <ProjectCard projectData={Project3}/>
      <img className='fixed -z-10 bottom-0' src="https://www.freepnglogos.com/uploads/mountain-png/mountain-png-uplyft-creative-branding-design-development-fort-33.png" alt="" />
      </div>
      </div>
