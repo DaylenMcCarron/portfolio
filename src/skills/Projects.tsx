fa8a5fd94d9e17e65e9736b6c1a357c511fa94db
Daylen McCarron <daylenricardomccarron@gmail.com>
2024-06-01 21:39:18 +0530

    add: made website responsive for mobile

--- src/skills/Projects.tsx
+++ src/skills/Projects.tsx
@@ -8,12 +8,12 @@ function Projects({}: Props) {
   const Project2 = {name:"The Coffee Place - (Live Website)",description:"The Coffee Place is an online store where users can purchase coffee. ",link:"https://thecoffeeplace.vercel.app",technology:"The website is built using HTML, CSS, JavaScript, Tailwind, and React.",imageName:"tcp"};
   const Project3 = {name:" QuickNoms - Q-Commerce Platfrom - (College Project)",description:"QuickNoms is a Q-Commerce platform that consists of a user, restaurant and delivery partner app.",link:"https://github.com/DaylenMcCarron/QuickNoms",technology:"It has been created using Flutter and Dart for the app, Firebase RTDB, Firestore, FCM, Firebase Cloud Storage",imageName:"qn"};
   return (
-    <div className='h-fit w-[100vw]'><h1 className='text-center text-6xl text-sky-200 sixtyfour py-2 shadow-white pt-12' id='Projects'>Projects</h1>
+    <div className='h-fit w-[100vw]'><h1 className='text-center text-5xl md:text-6xl text-sky-200 sixtyfour py-2 shadow-white pt-12' id='Projects'>Projects</h1>
      <div className='flex w-full h-fit  flex-col  pt-8'>
      <ProjectCard projectData={Project1}/>
      <ProjectCard projectData={Project2}/>
      <ProjectCard projectData={Project3}/>
-     <img className='fixed -z-10 bottom-0' src="https://www.freepnglogos.com/uploads/mountain-png/mountain-png-uplyft-creative-branding-design-development-fort-33.png" alt="" />
+     <img className='fixed -z-10 bottom-0 h-[40%] md:h-fit' src="https://www.freepnglogos.com/uploads/mountain-png/mountain-png-uplyft-creative-branding-design-development-fort-33.png" alt="" />
      </div>
      </div>
   )
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -3,19 +3,19 @@ type Props = {}
 
 function Skills({}: Props) {
   return (
-    <div className='w-full h-[70vh]'>
+    <div className='w-full h-fit'>
       <div className=' w-full h-full'>
-        <h1 className='text-center text-6xl text-sky-200 sixtyfour py-2 pt-12' id="Skills">Skills</h1>
-         <div className=' border-[1px] border-slate-700 glassbg shadow-sm shadow-slate-500 rounded-xl h-3/4 mt-3 m-auto w-[95%] text-white flex'>
+        <h1 className='text-center text-5xl md:text-6xl text-sky-200 sixtyfour py-2 pt-12' id="Skills">Skills</h1>
+         <div className=' border-[1px] border-slate-700 glassbg shadow-sm shadow-slate-500 rounded-xl h-3/4 mt-3 m-auto w-[95%] text-white md:flex'>
           <div className='basis-[30%]'>
             <img src="/assets/interface.svg" className="scale-75" alt="" />
           </div>
-          <div className='basis-[5%] flex flex-col'>
+          <div className='basis-[5%] flex md:flex-col '>
             <div className="  h-[50%] border-[1px] border-slate-200 hover:border-slate-200 shadow-slate-400 glassbg transition-all duration-200 ">
-              <h1 className=" relative top-[50%] -rotate-90 text-xl text-nowrap font-bold">Front-end</h1>
+              <h1 className=" relative top-[50%] md:-rotate-90 text-xl text-nowrap font-bold">Front-end</h1>
             </div>
             <div className="bg-glass h-[50%] border-[1px] border-slate-600 hover:border-slate-200 shadow-slate-400  transition-all duration-200">
-              <h1 className=" relative top-[50%] -rotate-90 text-xl text-nowrap">Back-end</h1>
+              <h1 className=" relative top-[50%] md:-rotate-90 text-xl text-nowrap">Back-end</h1>
             </div>
           </div>
           <div className='basis-[20%]'>
